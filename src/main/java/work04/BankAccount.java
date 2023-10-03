c3a919cfe072ffc966dcfcc234c025e0335b0287
unknown <you@example.com>
2023-10-03 19:15:32 +0700

    Initial commit

---
+++ src/main/java/work04/BankAccount.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package work04;
+
+/**
+ *
+ * @author Advice
+ */
+public class BankAccount {
+    private int number;
+    private Person owner;
+    private double balance;
+    
+    public BankAccount(int number, Person owner){
+        this.number=number;
+        this.owner=owner;
+    }
+    public Person getOwner(){
+        return owner;
+    }
+    public double getbalance(){
+        return balance;
+    }
+    public double deposit(double amount){
+        balance = balance + amount;
+        return balance;
+    }
+    public double withdraw(double amount){
+        balance = balance - amount;
+        return balance;
+    }
+    public void tranfer(double amount,BankAccount account){
+        balance = balance - amount;
+        account.deposit(amount);
+    }
+    @Override
+    public String toString(){
+        return "BankAccount" + owner;
+    }
+}
